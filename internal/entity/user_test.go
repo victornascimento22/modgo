9fcd12687392b3e22a8166958e3da97c2449c757
victor nascimento <nascimentovictor22@gmail.com>
2023-11-13 22:55:14 -0300

    fix bugs

--- internal/entity/user_test.go
+++ internal/entity/user_test.go
@@ -7,17 +7,17 @@ import (
 )
 
 func TestNewUser(t *testing.T) {
-	user, err := NewUser("John Doe", "j0j.com", "123456")
+	user, err := NewUser("John Doe", "j@j.com", "123456")
 	assert.Nil(t, err)
 	assert.NotNil(t, user)
 	assert.NotEmpty(t, user.ID)
 	assert.NotEmpty(t, user.Password)
 	assert.Equal(t, "John Doe", user.Name)
-	assert.Equal(t, "j0j.com", user.Email)
+	assert.Equal(t, "j@j.com", user.Email)
 }
 
 func TestUser_ValidatePassword(t *testing.T) {
-	user, err := NewUser("John Doe", "j0j.com", "123456")
+	user, err := NewUser("John Doe", "j@j.com", "123456")
 	assert.Nil(t, err)
 	assert.True(t, user.ValidatePassword("123456"))
 	assert.False(t, user.ValidatePassword("1234567"))
